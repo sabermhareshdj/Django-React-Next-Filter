7611d955051c755756a95d7c0eac079b967c9f9e
sabermhareshdj <dj.saber.mharesh@gmail.com>
2024-07-14 22:02:42 +0300

    design frontend ProductList

--- productsFilter/src/components/ProductList.jsx
+++ productsFilter/src/components/ProductList.jsx
@@ -1,16 +1,30 @@
-
-
 const ProductList = () => {
-  //logic
-
-
+  // logic
 
   // ui + logic 
-  return(
-    <div>
-      <h3> Product List </h3>
+  return (
+    <div className="">
+      <h3>Product List</h3>
+      <div className="row mt-5">
+        <div className="col-lg-4">
+          <div className="card">
+            <img
+              src="https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.large.jpg"
+              className="card-img-top"
+              alt="Product"
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                1200$
+              </p>
+              <a href="#" className="btn btn-primary">add to cart</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProductList
+export default ProductList;
