721207ee1b4bea9502ad8cc3f74e673da9313c3a
sabermhareshdj <dj.saber.mharesh@gmail.com>
2024-07-15 11:48:57 +0300

    add API code UI P1

--- productsFilter/src/api.jsx
+++ productsFilter/src/api.jsx
@@ -1,1 +1,17 @@
 // used to get data from api
+
+import axios from 'axios'
+
+const API_URL = "http://127.0.0.1:8000/api/"
+
+export const fetchProducts = async () => {
+  const response = await axios.get(`${API_URL}/products/`)
+  return response.data
+
+}
+
+export const fetchCategory = async () => {
+  const response = await axios.get(`${API_URL}/Category/`)
+  return response.data
+  
+}
