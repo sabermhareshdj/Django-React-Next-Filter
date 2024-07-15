1a16c7ef5b5b9a4af4e3e5091e15b1538e635d8e
sabermhareshdj <dj.saber.mharesh@gmail.com>
2024-07-15 11:57:08 +0300

    add zustand store P1

--- productsFilter/src/store.jsx
+++ productsFilter/src/store.jsx
@@ -1,1 +1,16 @@
 // zustand store for state managment
+
+import { create } from 'zustand'
+
+
+
+const productsStore = create(set => ({
+  products:[],
+  categories:[],
+  filters:{
+    categories:[],
+    price_range:[0,5000],
+  },
+}))
+
+export default productsStore;
